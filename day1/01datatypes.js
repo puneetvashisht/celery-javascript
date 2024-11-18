70bacdea637b4ecdf7a88803a8f53f2821c740e8
puneetvashisht <puneetvashisht@gmail.com>
2024-11-18 09:33:15 +0530

    first commit

---
+++ day1/01datatypes.js
@@ -0,0 +1,12 @@
+console.log("hello world!");
+
+
+// data types 
+let myNumber = 10;
+let myString = "Hello World!";
+let myBoolean = true;
+let myUndefined = undefined;
+let myNull = null;
+// let mySymbol = Symbol("my symbol");
+
+console.log(typeof(myNull));
