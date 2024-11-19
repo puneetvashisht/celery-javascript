95081b19d3ddd05e8c2296395411412504346f98
puneetvashisht <puneetvashisht@gmail.com>
2024-11-19 12:06:05 +0530

    exercise solution

--- day2/exercise.js
+++ day2/exercise.js
@@ -1,8 +1,37 @@
 
-// fetchWithRetry(url, retries=3)
-// create promise and return it
-// setTimeout Math.random() < 0.5
+function fetchWithRetry(url, retries=3){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=> {
+            const value = Math.random();
+            console.log(`Attempt ${retries} value ${value}`);
+            if(value < 0.5){
+                resolve(`Fetched data from ${url}`)
+            }
+            else{
+                reject(`Failed to fetch data from ${url}`)
+            }
+            
+        },0)  
+    }).catch(error => {
+        if(retries > 0){
+            return fetchWithRetry(url, retries-1)
+        }
+        else{
+            throw error
+        }
+    })
+}
 
+async function fetchDataWithRetry(){
+    try{
+        const data = await fetchWithRetry("/api/data")
+        console.log(data)
+    }
+    catch(error){
+        console.log('Error: ',error);
+    }
+    
+}
 
-//use .then or async/await
-// fetchWithRetry('/api/data', 3)
+fetchDataWithRetry();
+
