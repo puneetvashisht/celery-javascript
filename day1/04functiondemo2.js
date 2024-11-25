11c7eede54377a8525a4147195d1a58506c8a0d9
puneetvashisht <puneetvashisht@gmail.com>
2024-11-25 11:57:55 +0530

    execution context

--- day1/04functiondemo2.js
+++ day1/04functiondemo2.js
@@ -3,14 +3,15 @@
 
 // 2. Execution outer[x: 2, inner: fn]
 // inner[x: undef]
-
+// var x = 45
 function outer(){
     console.log('outer fn invoked...')
-    var x = 2;
+    // var x = 2;
     var inner = function(){
         console.log('inner fn invoked...')
-        console.log('x:', x + 2);
         var x = 10
+        console.log('x:', x + 2);
+       
     }
     return inner;
 }
---
+++ day5/execution-context-demo.js
@@ -0,0 +1,62 @@
+// // 1. Creation Global[a: undef, b: fn]
+// inner[x: undef]
+
+// 2. Execution Phase
+// [a: 'Some value', b: fn]
+
+
+
+// Global Execution Context
+// var a = 'x';
+// b();
+
+// function b(){
+//     // Function Execution Context for b()
+
+//     var a = 'y';
+//     console.log('In B', a)
+//     console.log('b function called...');
+// }
+
+// console.log(a);
+// var a  = 'Some value';
+
+
+// 1. Creation Phase
+// global [a:fn, b:fn, myVar:undef ]
+// a[myVar: undef]
+// b[]
+
+// 2. Execution 
+// global [a:fn, b:fn, myVar:1 ]
+// a[myVar: 2]
+// b[]
+
+function b(){
+    let myVar = 10
+    console.log(myVar);
+    
+}
+
+function a(){
+    var myVar = 2
+    b()
+}
+
+
+
+var myVar = 1;
+a();
+console.log('Global ', myVar);
+
+
+
+
+
+
+
+// console.log(window)
+
+// console.log(this)
+
+
